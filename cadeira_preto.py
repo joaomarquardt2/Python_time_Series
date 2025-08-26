18ee2160ff766cebfaf55c21cb57a91786f7b3b0
João Marquardt <joaovitormarquardt1@gmail.com>
2025-08-25 21:12:45 -0300

    Update and rename Cadeira_Preto.py to cadeira_preto.py

---
+++ cadeira_preto.py
@@ -0,0 +1,6 @@
+O header file declara informações que podem ser usadas em diferentes partes do programa (mais comum em C/C++).
+A docstring descreve o funcionamento de partes específicas do código no Python.
+def cadeira():
+    """Exibe uma mensagem cadeira"""
+    print("casdeira")
+
